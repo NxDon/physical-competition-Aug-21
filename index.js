7bee401c8cad572dff211205208c2c05109374a6
NxDon <645104928@qq.com>
2017-08-22 17:30:44 +0800

    refactor code for immutable

--- index.js
+++ index.js
@@ -4,21 +4,17 @@
 注意：理解题意（不是找出在两数组中都存在的元素）
 例如：nums1 = [1, 2, 2, 1], nums2 = [2, 2], 返回 [2, 2].*/
 
-function getIntersection(...arg) {
-    [arr1,arr2] = arg;
+function getIntersection(arr1,arr2) {
+    ar1 = arr1.concat();
+    ar2 = arr2.concat();//for immutable
     let result = [];
-    for(let i = 0; i < arr1.length; i++){
-        if(arr2.includes(arr1[i])){
-            result.push(arr1[i]);
-            arr2.splice(arr2.indexOf(arr1[i]),1);
-            arr1.splice(i,1);
+    for(let i = 0; i < ar1.length; i++){
+        if(ar2.includes(ar1[i])){
+            result.push(ar1[i]);
+            ar2.splice(ar2.indexOf(ar1[i]),1);
+            ar1.splice(i,1);
             i--;
         }
     }
     return result;
 }
-
-nums1 = [1, 2, 2, 1], nums2 = [2, 2];
-console.log(getIntersection(nums1,nums2));
-
-
